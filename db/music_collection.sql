1ca901bc7560936717a08985732a028fdef88256
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-26 20:44:51 +0100

    create table albums

--- db/music_collection.sql
+++ db/music_collection.sql
@@ -4,3 +4,11 @@ CREATE TABLE artists(
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255)
 );
+
+CREATE TABLE albums(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  title VARCHAR(255),
+  genere VARCHAR(255),
+  artist_id REFERENCES artists(id)
+);
